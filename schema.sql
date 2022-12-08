d27c784031914f77d7e2c5c90a1f17524a553ed5
Mauricio Mendoza <rbreva@gmail.com>
2022-12-07 22:06:30 -0500

    Vet clinic database: query multiple tables

--- schema.sql
+++ schema.sql
@@ -11,3 +11,33 @@ CREATE TABLE animals (
 );
 
 ALTER TABLE animals ADD species VARCHAR(50);
+
+/*Create a table named owners*/
+
+CREATE TABLE owners (  
+  id INT GENERATED ALWAYS AS IDENTITY,
+  full_name VARCHAR(250),
+  age INT,
+  PRIMARY KEY (id)
+);
+
+/*Create a table named species*/
+
+CREATE TABLE species (  
+  id INT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(250),
+  PRIMARY KEY (id)
+);
+
+/*Modify animals table*/
+
+  /*Remove column species*/
+ALTER TABLE animals DROP species;
+
+  /*Add column species_id which is a foreign key referencing species table*/
+ALTER TABLE animals ADD species_id INT;
+ALTER TABLE animals ADD FOREIGN KEY (species_id) REFERENCES species(id);
+
+  /*Add column owner_id which is a foreign key referencing the owners table*/
+ALTER TABLE animals ADD owner_id INT;
+ALTER TABLE animals ADD FOREIGN KEY (species_id) REFERENCES owners(id);
